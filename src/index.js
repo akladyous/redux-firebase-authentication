a078ff5654f1b7bc729954ff97e697e59e182d64
Boula Akladyous <akladyous@gmail.com>
2022-05-16 20:58:43 -0400

    redux - add provider to index main app

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-// react 18 format
-import App from "./App";
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { store } from "./store/store.js";
+import { Provider } from "react-redux";
+
 const root = createRoot(document.getElementById("root"));
-root.render(<App tab="home" />);
+root.render(
+    <BrowserRouter>
+        <React.StrictMode>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </React.StrictMode>
+    </BrowserRouter>
+);
