e21f53d32feeff8eaa52d75ae1a3dede07a2b7b9
Boula Akladyous <akladyous@gmail.com>
2022-05-16 20:52:24 -0400

    user authentication - logout component

---
+++ src/users/UsersLogout.js
@@ -0,0 +1,63 @@
+// import { useNavigate } from "react-router-dom";
+import { userSignOut } from "../auth/useAuthentication.js";
+import { userState } from "../features/users/userSlice.js";
+import { useSelector, useDispatch } from "react-redux";
+
+export default function UsersLogout() {
+    const dispatch = useDispatch();
+    const state = useSelector(userState);
+    // const navigate = useNavigate();
+
+    const handleSignOut = async (e) => {
+        e.preventDefault();
+        dispatch(userSignOut());
+        // navigate('/')
+    };
+
+    return (
+        <div className="container my-3">
+            <div className="row justify-content-md-center">
+                <div className="col col-lg-5 col-md-5 col-sm-5">
+                    <div className="card">
+                        <div className="card-header text-center">
+                            Logout Page
+                        </div>
+                        <div className="card-body">
+                            <div className="d-flex align-items-center">
+                                <img
+                                    src={require("../assets/images/avatar.jpeg")}
+                                    className="card-img-top mx-auto"
+                                    alt="avatar"
+                                    style={{ width: "25%", height: "25%" }}
+                                />
+                            </div>
+                            <div className="mb-2">
+                                <p
+                                    disabled
+                                    className="text-center border-0 form-control"
+                                    aria-describedby="response"
+                                >
+                                    {state.isAuthenticated
+                                        ? `Last login at ${new Date(parseInt(state.user.reloadUserInfo.lastLoginAt)).toLocaleString()}`
+                                        : "user not logged in"}
+                                </p>
+                            </div>
+
+                            <div className="row justify-content-center">
+                                <div className="col col-auto">
+                                    <button
+                                        disabled={!state.isAuthenticated}
+                                        className="btn btn-light"
+                                        onClick={handleSignOut}
+                                    >
+                                        Logout
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
