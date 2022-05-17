7147bbc3a62c77c9638e52e336d2df20a410590b
Boula Akladyous <akladyous@gmail.com>
2022-05-16 20:51:48 -0400

    user authentication - login component

---
+++ src/users/UsersLogin.js
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { userLogin } from "../auth/useAuthentication.js";
+import { userState } from "../features/users/userSlice.js";
+// import { useNavigate } from "react-router-dom";
+
+export default function UsersLogin() {
+    const dispatch = useDispatch();
+    const state = useSelector(userState);
+
+    // const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const handleForm = async (e) => {
+        e.preventDefault();
+        dispatch(userLogin({ email, password }));
+        // if (state.isAuthenticated) {
+        //     setTimeout(() => {
+        //         navigate('/')
+        //     }, 3000);
+        // }
+    };
+    
+    return (
+        <div className="container my-3">
+            <div className="row justify-content-md-center">
+                <div className="col col-lg-5 col-md-5 col-sm-5">
+                    <div className="card">
+                        <div className="card-header text-center">
+                            SignIn Page
+                        </div>
+                        <div className="card-body">
+                            <div className="d-flex align-items-center">
+                                <img
+                                    src={require("../assets/images/avatar.jpeg")}
+                                    className="card-img-top mx-auto"
+                                    alt="avatar"
+                                    style={{ width: "25%", height: "25%" }}
+                                />
+                            </div>
+                            <form onSubmit={handleForm}>
+                                <div className="mb-2">
+                                    <label
+                                        htmlFor="email"
+                                        className="form-label"
+                                    >
+                                        Email address
+                                    </label>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        className="form-control"
+                                        placeholder=""
+                                        value={email}
+                                        onChange={(e) =>
+                                            setEmail(e.target.value)
+                                        }
+                                    />
+                                </div>
+                                <div className="mb-2">
+                                    <label htmlFor="password">Password</label>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        className="form-control"
+                                        placeholder=""
+                                        value={password}
+                                        onChange={(e) => {
+                                            setPassword(e.target.value);
+                                        }}
+                                    />
+                                </div>
+                                <div className="mb-2">
+                                    <p
+                                        disabled
+                                        className="text-center border-0 form-control"
+                                        aria-describedby="response"
+                                    >
+                                        {
+                                            state.isAuthenticated
+                                            ? "login successfully completed"
+                                            : state.error
+                                        }
+                                    </p>
+                                </div>
+                                <div className="row justify-content-center">
+                                    <div className="col col-auto">
+                                        <button
+                                            type="submit"
+                                            className="btn btn-light"
+                                            disabled={state.isAuthenticated}
+                                        >
+                                            Submit
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
