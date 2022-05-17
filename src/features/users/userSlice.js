6a60c6748537b9420639e48090aa8033035b61e6
Boula Akladyous <akladyous@gmail.com>
2022-05-16 20:54:55 -0400

    refux - create users slice

---
+++ src/features/users/userSlice.js
@@ -0,0 +1,86 @@
+import {
+    createSlice,
+    isPending,
+    isRejected,
+    isFulfilled,
+} from "@reduxjs/toolkit";
+import {
+    userLogin,
+    userSignOut,
+    userSignUp,
+} from "../../auth/useAuthentication.js";
+
+const initialState = {
+    isAuthenticated: false,
+    user: null,
+    status: "idle", // idle | loading | succeeded | failed
+    error: "",
+    message: "",
+};
+
+const isPendingAction = isPending(userLogin, userSignOut, userSignUp);
+const isRejectedAction = isRejected(userLogin, userSignOut, userSignUp);
+const isFulfilledAction = isFulfilled(userLogin, userSignOut, userSignUp);
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        setUserStatus: (state, action) => {
+            state.isAuthenticated = action.payload;
+        },
+        setUser: (state, action) => {
+            state.user = action.payload;
+        },
+        setMessage: (state, action) => {
+            state.message = action.payload;
+        },
+        resetMessage: (state) => {
+            state.message = "";
+        },
+        setError: (state, action) => {
+            state.error = action.payload;
+        },
+        resetError: (state) => {
+            state.error = "";
+        },
+        resetState: () => {
+            return { ...initialState };
+        },
+    },
+    extraReducers(builder) {
+        builder
+            .addCase(userLogin.fulfilled, (state, action) => {
+                state.isAuthenticated = true;
+                state.error = "";
+                state.message = "Login successfullt completed";
+                state.user = { ...action.payload.user };
+            })
+            .addCase(userLogin.rejected, (state, action) => {
+                state.isAuthenticated = false;
+            })
+            .addCase(userSignOut.fulfilled, (state) => {
+                state.user = null;
+                state.isAuthenticated = false;
+            })
+            .addCase(userSignUp.fulfilled, (state, action) => {
+                state.isAuthenticated = true;
+                state.message = "Account successfully created";
+                state.user = { ...action.payload.user };
+            })
+            .addMatcher(isPendingAction, (state) => {
+                state.status = "loading";
+            })
+            .addMatcher(isFulfilledAction, (state, action) => {
+                state.status = "succeeded";
+            })
+            .addMatcher(isRejectedAction, (state, action) => {
+                state.status = "failed";
+                state.error = action.error.message;
+            });
+    },
+});
+export const userState = (state) => state.user;
+export const { setUserStatus, setUser, resetState, setMessage, setError } =
+    userSlice.actions;
+export default userSlice.reducer;
