7e5b1670ed72c2f728539f20e115c4f5289b0dbb
Boula Akladyous <akladyous@gmail.com>
2022-05-18 13:27:52 -0400

    test functionality

--- src/users/UsersHome.js
+++ src/users/UsersHome.js
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom"
-import { getCurrentUser } from '../auth/getCurrentUser.js'
 
 export default function UsersHome() {
     
     return (
         <>
-            <button onClick={()=>{console.log(getCurrentUser())}}>test</button>
             <Outlet />
         </>
     );
